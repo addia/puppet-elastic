9f66e6f1d400d6c727c3ba48f0345824b42d3538
Addi Abel <addi.abel@digital.landregistry.gov.uk>
2016-06-13 11:56:31 +0100

    fixed the elk ca cert config

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,78 +37,78 @@
 #
 #
 class elastic (
-  $clustername        = hiera('elk_stack_elastic_clustername'),
-  $cluster_servers    = hiera('elk_stack_elastic_servers'),
-  $version            = '2.3.2',
-  $repo_version       = '2.x',
-  $repo_manage        = true,
-  $auto_upgrade       = false,
-  $java_manage        = true,
-  $java_pkg           = 'java-1.8.0-openjdk',
-  $elastic_cert       = '/etc/elasticsearch/ssl/elastic.crt',
-  $elastic_key        = '/etc/elasticsearch/ssl/elastic.key',
-  $data_dir           = '/var/lib/es-data'
-
-  ){
+  $clustername                 = hiera('elk_stack_elastic_clustername'),
+  $cluster_servers             = hiera('elk_stack_elastic_servers'),
+  $version                     = '2.3.2',
+  $repo_version                = '2.x',
+  $repo_manage                 = true,
+  $auto_upgrade                = false,
+  $java_manage                 = true,
+  $java_pkg                    = 'java-1.8.0-openjdk',
+  $elastic_ca_cert             = '/etc/pki/ca-trust/source/anchors/elk_ca_cert.crt',
+  $elastic_cert                = '/etc/elasticsearch/ssl/elastic.crt',
+  $elastic_key                 = '/etc/elasticsearch/ssl/elastic.key',
+  $data_dir                    = '/var/lib/es-data'
+){
 
   notify { "## --->>> Installing and configuring ${clustername}": }
 
   class { 'elasticsearch':
-    version           => $version,
-    manage_repo       => $repo_manage,
-    repo_version      => $repo_version,
-    autoupgrade       => $auto_upgrade,
-    java_install      => $java_manage,
-    java_package      => $java_pkg,
-    datadir           => $data_dir,
-    config            => {
-      'cluster.name' =>  $clustername,
+    version                    => $version,
+    manage_repo                => $repo_manage,
+    repo_version               => $repo_version,
+    autoupgrade                => $auto_upgrade,
+    java_install               => $java_manage,
+    java_package               => $java_pkg,
+    datadir                    => $data_dir,
+    config                     => {
+      'cluster.name'           =>  $clustername,
       'discovery.zen.ping.multicast.enabled' => false,
       'discovery.zen.ping.unicast.hosts'     => $cluster_servers,
-      'network.host' =>  $::ipaddress_eth1
+      'network.host'           =>  $::ipaddress_eth1
+      }
     }
-  }
 
   elasticsearch::instance { "ops-els":
-    ssl               => true,
-    ca_certificate    => "/etc/pki/ca-trust/source/anchors/elk_ca.crt",
-    certificate       => $elastic_cert,
-    private_key       => $elastic_key,
-    keystore_path     => undef,
-    keystore_password => "keystore_pass",
-  }
+    ssl                        => true,
+    ca_certificate             => $elastic_ca_cert,
+    certificate                => $elastic_cert,
+    private_key                => $elastic_key,
+    keystore_path              => undef,
+    keystore_password          => "keystore_pass",
+    }
 
   elasticsearch::plugin{ 'mobz/elasticsearch-head':
-    instances  => 'ops-els'
-  }
+    instances                  => 'ops-els'
+    }
   
   elasticsearch::plugin{ 'lmenezes/elasticsearch-kopf':
-    instances  => 'ops-els'
-  }
+    instances                  => 'ops-els'
+    }
   
   file { "/etc/elasticsearch/ssl" :
-    ensure            => 'directory',
-    owner             => 'root',
-    group             => 'root',
-    mode              => '0755',
-  }
+    ensure                     => 'directory',
+    owner                      => 'root',
+    group                      => 'root',
+    mode                       => '0755',
+    }
 
   file { $elastic_key:
-    ensure            => file,
-    owner             => 'root',
-    group             => 'root',
-    mode              => '0644',
-    content           => hiera('elk_stack_elastic_key')
-  }
+    ensure                     => file,
+    owner                      => 'root',
+    group                      => 'root',
+    mode                       => '0644',
+    content                    => hiera('elk_stack_elastic_key')
+    }
 
   file { $elastic_cert:
-    ensure            => file,
-    owner             => 'root',
-    group             => 'root',
-    mode              => '0644',
-    content           => hiera('elk_stack_elastic_cert')
-  }
+    ensure                     => file,
+    owner                      => 'root',
+    group                      => 'root',
+    mode                       => '0644',
+    content                    => hiera('elk_stack_elastic_cert')
+    }
 
-}
+  }
 
 # vim: set ts=2 sw=2 et :
