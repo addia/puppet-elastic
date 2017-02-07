24b2dffff7eab17b868d377d5b3802abde2d799a
Addi Abel <addi@abel.de.com>
2017-02-07 13:31:52 +0000

    initial elastic 5.x upgrade

--- Vagrantfile
+++ Vagrantfile
@@ -1,18 +1,51 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-Vagrant.configure(2) do |config|
+Vagrant.require_version ">= 1.7.4"
 
-  config.vm.define :addis_virt1 do |addis_virt1|
-    addis_virt1.vm.box = "centos64"
-    addis_virt1.vm.network :private_network, :ip => '192.168.122.51'
+Vagrant.configure(2) do |conf|
+  conf.vm.box = "landregistry/centos"
+  conf.vm.synced_folder ".yum", "/var/cache/yum"
+  conf.vm.synced_folder ".gem", "/usr/local/share/gems/cache/"
+
+  conf.vm.provision "shell", inline: <<-SCRIPT
+    sed -i -e 's,keepcache=0,keepcache=1,g' /etc/yum.conf
+    yum install -y git
+    puppet module install puppetlabs-stdlib
+    puppet module install elasticsearch-elasticsearch
+    puppet module install ceritsc-yum
+    puppet module install puppetlabs-apt
+    puppet module install puppetlabs-java
+    puppet module install puppetlabs-java_ks
+    puppet module install richardc-datacat
+    puppet module install pcfens-ca_cert --version 1.3.0
+    puppet module install camptocamp-openssl
+    cd /etc/puppet/
+    ln -s /vagrant/hiera.vagrant.yaml /etc/puppet/hiera.yaml
+    cd /etc/puppet/modules
+    ln -s /vagrant /etc/puppet/modules/elastic
+    puppet apply /vagrant/tests/init.pp
+  SCRIPT
+
+  conf.vm.define "server8" do |web|
+    web.vm.host_name = "server8"
+    web.vm.network "private_network", :ip => "192.168.42.56"
   end
 
-  config.vm.provider :libvirt do |libvirt|
-    libvirt.driver = "qemu"
-    libvirt.host = "localhost"
-    libvirt.connect_via_ssh = true
-    libvirt.username = "root"
-    libvirt.storage_pool_name = "virt-pool"
+  conf.vm.define "server9" do |web|
+    web.vm.host_name = "server9"
+    web.vm.network "private_network", :ip => "192.168.42.57"
   end
+
+  conf.vm.define "server10" do |web|
+    web.vm.host_name = "server10"
+    web.vm.network "private_network", :ip => "192.168.42.58"
+  end
+
+  conf.vm.provider :virtualbox do |vb|
+    vb.customize ['modifyvm', :id, '--memory', ENV['VM_MEMORY'] || 2048]
+    vb.customize ['modifyvm', :id, '--vram', ENV['VM_VIDEO'] || 24]
+    vb.customize ["modifyvm", :id, "--cpus", ENV['VM_CPUS'] || 4]
+  end
+
 end
--- manifests/init.pp
+++ manifests/init.pp
@@ -10,22 +10,24 @@
 #
 # Document all Parameters:
 #
-#   version           = version to install
-#   repo_version      = yum repo version
-#   repo_manage       = 'true' for installing the yum.repo file
-#   auto_upgrade      = 'false' for NOT to autoupgrade ES
-#   java_manage       = 'true' for installing Java
-#   java_pkg          = which java package to install
-#   clustername       = the elasticsearch cluster name
-#   instance          = the elasticsearch instance name
-#   cluster_servers   = the servers addresses to configure the cluster
-#   keystore_dir      = leave as default
-#   keystore_passwd   = java keystore password
-#   ssl_cacert_file   = the CA certificate for self signed certs
-#   elastic_cert      = the certificate for the elastic cluster
-#   elastic_key       = the private for the elastic cluster
-#   data_dir          = directory for saving the ES data
-#   beats_dashboard   = the filename and version of the zip file to download and install
+#   version            = Elastic version to install
+#   repo_version       = Elastic rpm repo version
+#   repo_manage        = 'true' for letting puppet manage the repo file
+#   restart_on_change  = 'true' otherwise the resource will mask the process ...   :-(
+#   auto_upgrade       = 'false' for NOT EVER to autoupgrade Elastic
+#   java_install       = 'true' for installing the correct Java version
+#   clustername        = the elasticsearch cluster name
+#   instance           = the elasticsearch instance name
+#   cluster_servers    = the servers addresses to configure the cluster
+#   setup_housekeep    = set to true/undef to set-up houskeeping
+#   days_keep          = days to keep in the ELS database after houskeeping
+#   index_prefix       = the array of indices to run the housekeeping against
+#   keystore_dir       = leave as default
+#   keystore_passwd    = java keystore password
+#   ssl_cacert_file    = the CA certificate for self signed certs
+#   elastic_cert       = the certificate for the elastic cluster
+#   elastic_key        = the private for the elastic cluster
+#   data_dir           = directory for saving the ES data
 #
 #
 # ===========================
@@ -40,37 +42,107 @@
 # == Copyright
 # ------------
 #
-# Copyright:  ©  2016  LR / Addi.
+# Copyright:  ©  2017  LR / Addi.
 #
 #
 class elastic (
-  $version                     = $elastic::params::version,
-  $repo_version                = $elastic::params::repo_version,
-  $repo_manage                 = $elastic::params::repo_manage,
-  $auto_upgrade                = $elastic::params::auto_upgrade,
-  $java_manage                 = $elastic::params::java_manage,
-  $java_pkg                    = $elastic::params::java_pkg,
-  $clustername                 = $elastic::params::clustername,
-  $instance                    = $elastic::params::instance,
-  $cluster_servers             = $elastic::params::cluster_servers,
-  $keystore_dir                = $elastic::params::keystore_dir,
-  $keystore_passwd             = $elastic::params::keystore_passwd,
-  $ssl_cacert_file             = $elastic::params::ssl_cacert_file,
-  $elastic_cert                = $elastic::params::elastic_cert,
-  $elastic_key                 = $elastic::params::elastic_key,
-  $beats_dashboard             = $elastic::params::beats_dashboard,
-  $data_dir                    = $elastic::params::data_dir
+  $version            = '5.2.0',
+  $repo_version       = '5.x',
+  $repo_manage        = true,
+  $restart_on_change  = true,
+  $auto_upgrade       = false,
+  $java_install       = true,
+  $clustername        = hiera('elk_stack_elastic_clustername'),
+  $instance           = hiera('elk_stack_elastic_instance'),
+  $cluster_servers    = hiera('elk_stack_elastic_servers'),
+  $setup_housekeep    = hiera('elk_stack_do_housekeeping'),
+  $days_keep          = hiera('elk_stack_days_to_keep'),
+  $index_prefix       = hiera('elk_stack_index_prefix'),
+  $keystore_dir       = undef,
+  $keystore_passwd    = 'keystore_pass',
+  $ssl_cacert_file    = '/etc/pki/ca-trust/source/anchors/lr_rootca.crt',
+  $elastic_cert       = '/etc/elasticsearch/ssl/elastic.crt',
+  $elastic_key        = '/etc/elasticsearch/ssl/elastic.key',
+  $data_dir           = '/var/lib/elasticsearch',
 ) {
 
-  include elastic::params
+  notify { "## --->>> Installing and configuring ${clustername}": }
 
-  notify { "## --->>> configuring package ${clustername}": }
+  class { 'elasticsearch':
+    version           => $version,
+    manage_repo       => $repo_manage,
+    repo_version      => $repo_version,
+    restart_on_change => $restart_on_change,
+    autoupgrade       => $auto_upgrade,
+    java_install      => $java_install,
+    datadir           => $data_dir,
+    api_protocol      => 'https',
+    api_ca_file       => $ssl_cacert_file,
+    config            => {
+      'cluster.name'                     => $clustername,
+      'discovery.zen.ping.unicast.hosts' => $cluster_servers,
+      }
+    }
 
-    anchor { 'elastic::begin': } ->
-    class { '::elastic::install': } ->
-    class { '::elastic::config': } ~>
-    anchor { 'elastic::end': }
+  elasticsearch::instance { $instance:
+    ensure => 'present',
+    status => 'running',
+    }
 
-}
+  if $::setup_housekeep {
+    # Set-up a housekeeping job
+    file { '/usr/local/bin/remove_old_index.sh' :
+      ensure  => 'file',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => template('elastic/remove_old_index_sh.erb'),
+      }
 
+    # Set backup cron job
+    cron { 'els_housekeeping':
+      ensure  => present,
+      command => '/usr/local/bin/remove_old_index.sh',
+      hour    => 22,
+      minute  => 15,
+      user    => root
+      }
+    } else {
+    cron { 'els_housekeeping':
+      ensure => absent,
+      }
+    }
+
+  file { '/etc/elasticsearch/ssl' :
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    }
+
+  file { $elastic_key:
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => hiera('elk_stack_elastic_key')
+    }
+
+  file { $elastic_cert:
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => hiera('elk_stack_elastic_cert')
+    }
+
+  ca_cert::ca { 'lr_rootca':
+    ensure => 'trusted',
+    source => 'puppet:///modules/elastic/lr_rootca.crt',
+    }
+
+  }
+
+
+# -----------------------
 # vim: set ts=2 sw=2 et :
