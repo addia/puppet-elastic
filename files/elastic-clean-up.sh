a0f39185d2219d0a15c279fcd22013c38dce90bf
Addi Abel <addi@abel.de.com>
2017-03-15 07:36:26 +0000

    added clean-up script

---
+++ files/elastic-clean-up.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+updatedb
+systemctl status elastic*
+systemctl stop elastic*
+rm -rf /etc/elasticsearch /var/lib/elasticsearch /var/log/elasticsearch /var/lib/yum/repos/x86_64/7/elastic* /usr/share/elasticsearch
+rm -rf /etc/sysconfig/elastic*  /etc/systemd/system/elastic* /opt/elasticsearch /usr/lib/sysctl.d/elasticsearch.conf /usr/lib/systemd/system/elastic*  /usr/lib/tmpfiles.d/elasticsearch.conf
+updatedb
+locate elasticsearch
